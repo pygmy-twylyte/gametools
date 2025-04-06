9007f28c6069712281a549f2bdee9f44c3c0a18c
David Brown <dave@pop-top.local>
2025-04-05 23:51:00 -0500

    added top level example for cards.rs

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,30 @@
 // use gametools::dice::*;
 use gametools::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-   
-    /*
-    Dominos - first play demo
-     */
-    
-    // Create a new set of dominos, 0-12 pips on each side
-    let mut pile = BonePile::new(12);
-    // Draw 15 of those into a hand
-    let mut hand = DominoHand::new_with_draw("me", 15, &mut pile)?;
-    // First round starts from 12 (note 12|12 would be removed from player who had it in real game)
-    let start_from = 12;
-    // Find the longest starting line that can be played on start_from with this hand
-    let best_line_ids = hand.find_longest_from(start_from);
-    // Play that best line on the player's train
-    let mut p1_train = Train::new("me", false, start_from);
-    hand.play_line(&best_line_ids, &mut p1_train)?;
-    println!("{hand}");
-    println!("{p1_train}");
+fn main() -> GameResult<()> {
+   let mut deck = Deck::standard_52("main deck");
+   let mut hand = CardHand::new("player_1");
+   let mut discard_pile = Pile::new("discard");
 
-    Ok(())
+   // shuffle and draw 7 cards into the hand
+   deck.shuffle();
+   hand.draw_cards_from(&mut deck, 7)?;
+   println!("{hand}");
+
+   // or deal from the deck to multiple hands
+   let mut other_hands = vec![CardHand::new("player_2"), CardHand::new("player_3")];
+   deck.deal_to_hands(&mut other_hands, 7)?;
+
+   // count ranks and suits
+   let num_spades = hand.count_suit(Suit::Spades);
+   let num_queens = hand.count_rank(Rank::Queen);
+
+   // search for a card in a hand
+   let search_card = Card::new_temp(Rank::Ace, Suit::Clubs);
+   let go_fish = hand.contains(&search_card);
+
+   // move a card from hand to another hand or a pile; returns an error if card is not in hand
+   hand.transfer_card(&search_card,&mut discard_pile)?;
+
+   Ok(())
 }
